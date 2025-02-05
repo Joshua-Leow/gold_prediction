ae3537c24b5368ffc0436517ea24b967d92d0d90
woshileow <josh.leow.2010@gmail.com>
2025-02-06 01:09:22 +0800

    minor changes to params

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 symbol = 'GC=F'           # Trading symbol
 interval = '1h'           # Time interval
-confidence = 0.40         # Prediction confidence threshold
+confidence = 0.50         # Prediction confidence threshold
 target_candle = 7         # Future candle to predict
 profit_perc = 1.00        # Take profit percentage
 stop_loss_perc = 0.20     # Stop loss percentage
