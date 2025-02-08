72ca188d6cad26f7dba7cb2821e146e9747b79c4
woshileow <josh.leow.2010@gmail.com>
2025-02-08 20:37:08 +0800

    update best feature_horizons:
            Model: Random Forest
    feature_horizons = [1, 2, 4, 8, 16]:
            Precision: 0.4000
            Recall: 0.6300
            F1 Score: 0.4933
    feature_horizons = [2, 5, 60, 250, 1000]:
            Precision: 0.5159
            Recall: 0.7179
            F1 Score: 0.6002
    feature_horizons = [2, 8, 32, 128, 512]:
            Precision: 0.5825
            Recall: 0.7628
            F1 Score: 0.6604

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 import numpy as np
 
-symbol = 'GC=F'                      # Trading symbol
-# symbol = 'GRAPHITE.NS'             # Trading symbol
-interval = '1h'                      # Time interval
-confidence = 0.40                    # Prediction confidence threshold
-target_candle = 120                  # Future candle to predict
-profit_perc = 2.00                   # Take profit percentage
-stop_loss_perc = profit_perc/4       # Stop loss percentage
-gap_between_trades = 12              # Number of candles to wait before making the next trade
-feature_horizons = [1, 2, 4, 8, 16]  # Feature Horizons to be trained with
+symbol = 'GC=F'                          # Trading symbol
+# symbol = 'GRAPHITE.NS'                 # Trading symbol
+interval = '1h'                          # Time interval
+confidence = 0.40                        # Prediction confidence threshold
+target_candle = 120                      # Future candle to predict
+profit_perc = 2.00                       # Take profit percentage
+stop_loss_perc = profit_perc/4           # Stop loss percentage
+gap_between_trades = 12                  # Number of candles to wait before making the next trade
+feature_horizons = [2, 8, 32, 128, 512]  # Feature Horizons to be trained with
 
 
 def define_target_labels(df):
