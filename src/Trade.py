db985b5cb3c12f6cf3eaa042b9951b3ded0fe22d
woshileow <josh.leow.2010@gmail.com>
2025-02-12 14:11:42 +0800

    modified print of trades open and close

--- src/Trade.py
+++ src/Trade.py
@@ -253,7 +253,7 @@ def simulate_trades(df, predictions, initial_cash=10000, profit_perc=0.02, stop_
                     row_difference = abs(current_row - entry_row)
                     # Close trade if trade is opened for more than twice of target_candle duration
                     if row_difference > target_candle*2:
-                        print("\t\t\t\t\t\t\t\t\t\t\t", end=' ')
+                        print("\t\t", end=' ')
                         active_trade.close_trade(row.Close, row.name)
                         print(f"(Exceeded {target_candle*2} candles)")
                         cash += active_trade.profit
